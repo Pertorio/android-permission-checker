fa6632dba56037a07b38bd16d7dad9cf9da37e1b
Pertorio <narinthorn.chi@gmail.com>
2023-03-10 10:34:52 +0700

    textView_log update

--- app/src/main/java/com/example/ssscanner/MainActivity.java
+++ app/src/main/java/com/example/ssscanner/MainActivity.java
@@ -1,8 +1,9 @@
 package com.example.ssscanner;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +12,35 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //TEXTVIEW SECTION
+        TextView textView_log = (TextView) findViewById(R.id.textView_log);
+        setup_textView(textView_log);
+        print_banner(textView_log);
+
+
+
+
+
 
     }
+
+    protected void setup_textView(TextView textView){
+        textView.setMovementMethod(new ScrollingMovementMethod());
+
+    }
+
+    protected void print_banner(TextView textView){
+        textView.setText("");
+        textView.append("         _.._..,_,_\n");
+        textView.append("        (          )\n");
+        textView.append("         ]~,\"-.-~~[\n");
+        textView.append("       .=])' (;  ([\n");
+        textView.append("       | ]:: '    [\n");
+        textView.append("       '=]): .)  ([\n");
+        textView.append("         |:: '    |\n");
+        textView.append("          ~~----~~\n\n");
+        textView.append("SSScanner v.1.0.0\n");
+    }
+
+
 }
